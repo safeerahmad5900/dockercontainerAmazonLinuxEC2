0b91cade403fa0bbca9cf866b1601e2de2a8b3d2
safeer007 <safeerahmad5900@gmail.com>
2023-02-05 18:12:08 +0000

    Gvars

---
+++ Terraform/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "App"   = "Docker-EC2"
+    "Onwer" = "safeer007"
+  }
+}
+
+
+# Prefix to for resources
+output "prefix" {
+  value     = "Assignment1-Clo835"
+}
